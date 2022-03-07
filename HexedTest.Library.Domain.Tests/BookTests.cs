4cb85d9a91999b79275568f46001b189997e3031
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-07 12:38:43 -0400

    Add book test

---
+++ HexedTest.Library.Domain.Tests/BookTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Domain.Tests;
+
+public class BookTests
+{
+
+}
+
