6d189043e5be9bba43b55935a08e377432b8d1c9
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-07 19:51:19 -0400

    Add BorrowOrder & BorrowOrder.Create method

---
+++ HexedTest.Library.Domain.Tests/BorrowOrderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Domain.Tests;
+
+public class BorrowOrderTests
+{
+    [Fact]
+    public void Create_Should_CreateAValidBorrowOrder()
+    {
+        // Arrange
+        BorrowOrder order;
+        Guid userId = Guid.NewGuid();
+        string bookISBN = "978-1-0217-2611-7";
+
+        // Act
+        order = BorrowOrder.Create(userId, bookISBN);
+
+        // Assert
+        Assert.NotNull(order);        
+        Assert.Equal(userId, order.UserId);
+        Assert.Equal(bookISBN, order.BookISBN);
+        Assert.False(order.IsCopy, "Order is copy");
+        Assert.False(order.IsReturned, "Order is returned");
+        Assert.NotEqual(default, order.DateRequested);
+        Assert.Null(order.DateReturned);
+    }
+}
+
---
+++ HexedTest.Library.Domain/Entities/BorrrowOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Domain.Entities;
+
+public class BorrowOrder
+{
+    public Guid Id { get; init; }
+    public Guid UserId { get; init; }
+    public string BookISBN { get; init; }
+    public bool IsCopy { get; private set; } = false;
+    public bool IsReturned { get; private set; } = false;
+    public DateTime DateRequested { get; init; }
+    public DateTime? DateReturned { get; private set; }
+
+    internal BorrowOrder(Guid id, Guid userId, string bookISBN)
+    {
+        Id = id;
+        UserId = userId;
+        BookISBN = bookISBN;
+        DateRequested = DateTime.Now;
+    }
+
+    public static BorrowOrder Create(Guid userId, string bookISBN)
+    {
+        return new BorrowOrder(Guid.NewGuid(), userId, bookISBN);
+    }
+}
+
