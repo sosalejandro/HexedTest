50a9a1d8102faffda7cf4c90f871c645a3d98311
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:17:49 -0400

    Add repositories interfaces

---
+++ HexedTest.Library.Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using HexedTest.Library.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Domain.Repositories;
+
+public interface IBookRepository
+{
+    // no books = empty library - use case 1
+    // books in library = items in library - use case 2
+    Task<ICollection<Book>> GetAvailableBooks(CancellationToken cancellation);
+}
+
---
+++ HexedTest.Library.Domain/Repositories/ILibraryRepository.cs
@@ -0,0 +1,13 @@
+﻿using HexedTest.Library.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Domain.Repositories;
+public interface ILibraryRepository : IRepository<Entities.Library>
+{
+    Task AddLibrary(Entities.Library library, CancellationToken cancellation);
+}
+
