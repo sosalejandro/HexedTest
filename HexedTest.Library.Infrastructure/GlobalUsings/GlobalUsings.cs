9a4c8a9aa7c8930ece655d4ba4ef8440add0d0db
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 10:41:47 -0400

    Add Infrastructure

---
+++ HexedTest.Library.Infrastructure/GlobalUsings/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.Azure.Cosmos;
+global using Microsoft.EntityFrameworkCore;
