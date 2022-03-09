87a626ad6d96d14fb32521426f8c79037d465e73
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:19:16 -0400

    Add library db context

---
+++ HexedTest.Library.Infrastructure/LibraryContext.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Infrastructure
+{
+    public class LibraryContext : DbContext, IUnitOfWork
+    {
+        public const string DEFAULT_SCHEMA = "library";
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BorrowOrder> BorrowOrders { get; set; }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            return result == 1;
+        }
+    }
+}
