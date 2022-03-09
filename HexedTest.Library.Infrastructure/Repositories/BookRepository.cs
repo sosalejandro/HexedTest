7de8da88219f6051aeffb5ae915891b523581282
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:20:52 -0400

    Add repositories

---
+++ HexedTest.Library.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using HexedTest.Library.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Infrastructure.Repositories;
+
+public class BookRepository : IBookRepository
+{
+    private readonly LibraryContext Context;
+    public BookRepository(LibraryContext context)
+    {
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<ICollection<Book>> GetAvailableBooks(CancellationToken cancellation)
+    {
+        var availableBooks
+            = await Context.Books.Where(
+                b => b.Stock.OriginalAmount > 0 || b.Stock.CopiesAmount > 0)
+            .ToListAsync(cancellation);
+
+        return availableBooks;        
+    }
+}
+
---
+++ HexedTest.Library.Infrastructure/Repositories/LibraryRepository.cs
@@ -0,0 +1,35 @@
+﻿using HexedTest.Library.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Infrastructure.Repositories
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        private readonly LibraryContext Context;
+
+        public LibraryRepository(LibraryContext context)
+        {
+            Context = context;
+        }
+        public IUnitOfWork UnitOfWork
+        {
+            get
+            {
+                return Context;
+            }
+        }
+
+        public Task AddLibrary(Domain.Entities.Library library, CancellationToken cancellation = default)
+        {
+            Context.AddRange(library.Books, library.BorrowedBooks);
+
+            UnitOfWork.SaveChangesAsync(cancellation);
+
+            return Task.CompletedTask;
+        }
+    }
+}
