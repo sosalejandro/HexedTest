968c1af31db4bf1ac31138c1881c61bcb96d3bde
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:21:17 -0400

    Add EntityType configurations

---
+++ HexedTest.Library.Infrastructure/EntityConfiguration/BookEntityTypeConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Infrastructure.EntityConfiguration
+{
+    class BookEntityTypeConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> bookConfiguration)
+        {
+            bookConfiguration.ToTable("books", LibraryContext.DEFAULT_SCHEMA);
+
+            bookConfiguration.HasKey(b => b.ISBN);
+
+            bookConfiguration.Property(b => b.ISBN)
+                .IsRequired();
+
+            bookConfiguration.Property(b => b.Title)
+                .IsRequired();
+
+            bookConfiguration.Property(b => b.Author)
+                .IsRequired();
+
+            bookConfiguration.Property(b => b.YearPublished)
+                .IsRequired();
+
+            bookConfiguration
+                .OwnsOne(b => b.Stock, s =>
+                {
+                    s.Property<string>("BookISBN");
+                    s.WithOwner();
+                });
+        }
+    }
+}
---
+++ HexedTest.Library.Infrastructure/EntityConfiguration/BorrowOrderEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexedTest.Library.Infrastructure.EntityConfiguration
+{  
+    class BorrowOrderEntityTypeConfiguration : IEntityTypeConfiguration<BorrowOrder>
+    {
+        public void Configure(EntityTypeBuilder<BorrowOrder> borrowOrderConfiguration)
+        {
+            borrowOrderConfiguration.ToTable("books", LibraryContext.DEFAULT_SCHEMA);
+
+            borrowOrderConfiguration.HasKey(bo => bo.Id);
+
+            borrowOrderConfiguration.Property(bo => bo.Id)
+                .UseHiLo("borroworderseq", LibraryContext.DEFAULT_SCHEMA);
+
+            borrowOrderConfiguration.HasOne(typeof(Book))
+                .WithMany()
+                .HasForeignKey("BookISBN");
+
+            borrowOrderConfiguration.Property(bo => bo.UserId)
+                .IsRequired();
+
+            borrowOrderConfiguration.Property(bo => bo.IsCopy)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            borrowOrderConfiguration.Property(bo => bo.IsReturned)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            borrowOrderConfiguration.Property(bo => bo.DateRequested)
+                .IsRequired()
+                .HasDefaultValueSql("GETDATE()");
+
+            borrowOrderConfiguration.Property(bo => bo.DateReturned)
+                .HasDefaultValue(null);
+        }
+    }
+}
