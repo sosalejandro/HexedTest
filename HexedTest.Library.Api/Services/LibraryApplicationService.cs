639019cca7f56fc2373f23d79bd61a4803a65a22
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:47:25 -0400

    Add business logic

--- HexedTest.Library.Api/Services/LibraryApplicationService.cs
+++ HexedTest.Library.Api/Services/LibraryApplicationService.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using HexedTest.Library.Api.Commands;
 
 namespace HexedTest.Library.Api.Services
 {
@@ -9,6 +9,20 @@ namespace HexedTest.Library.Api.Services
         public LibraryApplicationService(IAggregateStore<Domain.Entities.Library> aggregateStore)
         {
             libraryAggregateStore = aggregateStore;
+        }
+
+        public async Task HandleAsync(BorrowBookCommand command)
+        {
+            var library = await libraryAggregateStore.LoadAsync();
+            library.BorrowBook(command.ISBN, command.UserId, command.Petition);
+            await libraryAggregateStore.SaveAsync(library);
+        }
+
+        public async Task HandleAsync(ReturnBookCommand command)
+        {
+            var library = await libraryAggregateStore.LoadAsync();
+            library.ReturnBook(command.ISBNs, command.UserId);
+            await libraryAggregateStore.SaveAsync(library);
         }
     }
 }
