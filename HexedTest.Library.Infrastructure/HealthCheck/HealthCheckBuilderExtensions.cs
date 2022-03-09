2f7ad3faffc836dd29b343be1fe621fd52f8fc9d
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:22:34 -0400

    Add HealthCheck implementation

---
+++ HexedTest.Library.Infrastructure/HealthCheck/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HexedTest.Library.Infrastructure.HealthCheck;
+
+public static class HealthCheckBuilderExtension
+{
+    public static IHealthChecksBuilder AddCosmosDbCheck(this IHealthChecksBuilder builder, IConfiguration configuration)
+    {
+        return builder.Add(new HealthCheckRegistration("TripsAgency", new LibraryCosmosDbHealthCheck(configuration), HealthStatus.Unhealthy, null));
+    }
+}
+
---
+++ HexedTest.Library.Infrastructure/HealthCheck/LibraryCosmosDbHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿namespace HexedTest.Library.Infrastructure.HealthCheck;
+public class LibraryCosmosDbHealthCheck : IHealthCheck
+{
+    private readonly CosmosClient cosmosClient;
+    private readonly IConfiguration configuration;
+
+    public LibraryCosmosDbHealthCheck(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        cosmosClient = new CosmosClient(configuration["CosmosDb:ConnectionString"]);
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        await cosmosClient.ReadAccountAsync();
+        var databaseId = configuration["CosmosDb:DatabaseId"];
+        var containerId = configuration["CosmosDb:ContainerId"];
+        var container = cosmosClient.GetContainer(databaseId, containerId);
+        var containerProperties = await container.ReadContainerAsync(cancellationToken: cancellationToken);
+        return containerProperties.StatusCode == System.Net.HttpStatusCode.OK ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy();
+    }
+}
