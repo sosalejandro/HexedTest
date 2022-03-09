3fb9659292ef6b6bb9b4fa45993846430f3bd63f
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 18:22:55 -0400

    Add new global imports

--- HexedTest.Library.Infrastructure/GlobalUsings/GlobalUsings.cs
+++ HexedTest.Library.Infrastructure/GlobalUsings/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Diagnostics.HealthChecks;
 global using Microsoft.Azure.Cosmos;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using HexedTest.Library.Domain.Entities;
+global using HexedTest.Library.Domain.SeedWork;
