cd6bf99c35e5a45df23b3030b41ee96d11a874df
Alejandro sosa <alejandrososadev@gmail.com>
2022-03-09 19:10:30 -0400

    Add controllers endpoints

---
+++ HexedTest.Library.Infrastructure/EntityConfiguration/LibraryEntityTypeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace HexedTest.Library.Infrastructure.EntityConfiguration;
+
+class LibraryEntityTypeConfiguration : IEntityTypeConfiguration<Domain.Entities.Library>
+{
+    public void Configure(EntityTypeBuilder<Domain.Entities.Library> libraryConfiguration)
+    {
+ 
+    }
+}
+
